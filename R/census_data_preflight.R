aa00d582a7c064a7b66e02d4fcb7909bd1ac7205
Eric Manning <ericmm@princeton.edu>
2024-04-21 21:49:05 -0400

    Update census_data_preflight.R
    removed check against incorrect package vars

--- R/census_data_preflight.R
+++ R/census_data_preflight.R
@@ -4,23 +4,5 @@
 #' @keywords internal
 
 census_data_preflight <- function(census.data, census.geo, year) {
-  vars_ <- unlist(census_geo_api_names(year = year))
-  legacy_vars <- unlist(census_geo_api_names_legacy(year = year))
   
-  test <- lapply(census.data, function(x) {
-    nms_to_test <- names(x[[census.geo]])
-    all(vars_ %in% nms_to_test) || all(legacy_vars %in% nms_to_test)
-  }) 
-  missings <- names(test)[!unlist(test)]
-  
-  if(any(!unlist(test))) {
-    stop(
-      paste0(
-        "Missing ", 
-        paste0(vars_, collapse = ", "), 
-        " from census.data object. Please update your census.data by",
-        " running `get_census_data` again."
-      )
-    )
-  }
 }
